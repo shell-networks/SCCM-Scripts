33daaa79c4d27cdca8020a3f0b0b98c6c3c8106a
Guillaume Mateos <gmateos@shell-networks.local>
2018-05-19 19:37:22 +0200

    Add inbound firewall rule creation

--- 05-Install-SQL-on-independant-server.ps1
+++ 05-Install-SQL-on-independant-server.ps1
@@ -9,7 +9,8 @@
     .SYNOPSIS
     Automatic Installation of SQL Server 2012 for SCCM
     .DESCRIPTION
-
+    Automatic Installation of SQL Server 2012 for SCCM
+    Create inbound firewall rules
 #>
 
 $SqlExePath = "D:\Setup.exe" #Indicate there where is stored the .exe
@@ -17,3 +18,6 @@ $SqlExePath = "D:\Setup.exe" #Indicate there where is stored the .exe
 $SccmSQLConfigFile = ".\05-SQL-ConfigurationFile.ini" #Here is the custom ini file, feel free to adapt to your needs
 
 & $SqlExePath /IACCEPTSQLSERVERLICENSETERMS /CONFIGURATIONFILE=$SccmSQLConfigFile /QUIETSIMPLE
+
+New-NetFirewallRule -Name "SQL SCCM INBOUND" -DisplayName "SQL SCCM INBOUND" -Description "All nescessary inbound ports for SCCM instance" `
+-Profile Domain -Direction Inbound -LocalPort "135","1433","4022" -Enabled True -Protocol TCP
